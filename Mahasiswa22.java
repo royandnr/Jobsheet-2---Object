666b3e60e126bab089d9ab1c9f113bdab6cb64c3
Yann <yann@gmail.com>
2026-02-18 10:41:59 +0700

    Percobaan 1

---
+++ Mahasiswa22.java
@@ -0,0 +1,36 @@
+public class Mahasiswa22 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+
+    void tampilkaninformasi () {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+    void ubahKelas (String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void updateIPK (double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0.");
+        }
+        
+    }
+    String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja Sangat Baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja Baik";
+        } else if (ipk >= 2.5) {
+            return "Kinerja Cukup";
+        } else {
+            return "Kinerja Kurang";
+        }
+    }
+
+}
